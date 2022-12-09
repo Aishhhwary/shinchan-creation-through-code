faec5e1137d5901391f88b2ff89f8e1e06635e95
Aishhhwary <119833560+Aishhhwary@users.noreply.github.com>
2022-12-09 11:25:01 +0530

    Add files via upload

---
+++ shinchan.py
@@ -0,0 +1,15 @@
+from turtle import *
+#import time
+s=Screen()
+s.screensize(700,1000)
+speed(5)
+def my_goto(x, y):
+    penup()
+    goto(x, y)
+    pendown()
+#time.sleep(10)
+#ht()
+pensize(2)
+def short():
+    fillcolor('#ffec40')
+    begin_fill()
